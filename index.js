b65ab48c4db7edb92ec7b0526448b57b3e6d669e
damian <damiankohut@gmail.com>
2021-10-27 14:57:44 +0000

    finish answering all the questions

--- index.js
+++ index.js
@@ -8,29 +8,42 @@ function betweenFiveAndTen(int){
 }
 //console.log(betweenFiveAndTen(8))
 //2
-function betweenTenAndThirty(){
-  
+function betweenTenAndThirty(int){
+   if ((int >=10) && (int <=30)){
+      return true 
+  }else {
+      return false
+  } 
 }
-
+//console.log(betweenTenAndThirty(15))
 //3
-function betweenThirtyAndFifty(){
-  
+function betweenThirtyAndFifty(int){
+   if ((int >=30) && (int <=50)){
+      return true 
+  }else {
+      return false
+  } 
 }
 
-
+//console.log(betweenThirtyAndFifty(35))
 //4
-function betweenThirtyFiftySeventy(){
-}
-
-//5
-function betweenTwentyFiftySixty(int){
-  if ((int >= 0) && (int <= 20) || (int >= 50) && (int <= 70)){
+function betweenThirtyFiftySeventy(int){
+    if ((int >= 0) && (int <= 30) || (int >= 50) && (int <= 70)){
     return true
   }else {
     return false
   }
 }
-console.log(betweenTwentyFiftySixty(23))
+//console.log(betweenThirtyFiftySeventy(25))
+//5
+function betweenTwentyFiftySixty(int){
+  if ((int >= 0) && (int <= 20) || (int >= 50) && (int <= 60)){
+    return true
+  }else {
+    return false
+  }
+}
+//console.log(betweenTwentyFiftySixty(23))
 //6
 function betweenTenTwentyThirty(int){
     if ((int >=0) && (int <=10) || (int >= 20) && (int <= 30)){
