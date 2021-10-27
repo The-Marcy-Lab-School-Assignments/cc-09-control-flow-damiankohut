edfaac9b1dc99cf372811ee0c2826d33ee40ca6b
damian <damiankohut@gmail.com>
2021-10-27 14:43:28 +0000

    finish some questions

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 //1
-function betweenFiveAndTen(){
-  
+function betweenFiveAndTen(int){
+  if ((int >=5) && (int <=10)){
+      return true 
+  }else {
+      return false
+  }
 }
-
+//console.log(betweenFiveAndTen(8))
 //2
 function betweenTenAndThirty(){
   
@@ -16,15 +20,25 @@ function betweenThirtyAndFifty(){
 
 //4
 function betweenThirtyFiftySeventy(){
-  
 }
 
 //5
-function betweenTwentyFiftySixty(){
-  
+function betweenTwentyFiftySixty(int){
+  if ((int >= 0) && (int <= 20) || (int >= 50) && (int <= 70)){
+    return true
+  }else {
+    return false
+  }
 }
-
+console.log(betweenTwentyFiftySixty(23))
 //6
-function betweenTenTwentyThirty(){
+function betweenTenTwentyThirty(int){
+    if ((int >=0) && (int <=10) || (int >= 20) && (int <= 30)){
+      return true 
+  }else {
+      return false
+      
+  }
   
 }
+//console.log(betweenTenTwentyThirty(21))
